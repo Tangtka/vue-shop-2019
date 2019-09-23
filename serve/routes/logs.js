3d40f5410db1933994234db4d0731fd2afa26de2
tangruijie <852057349@qq.com>
2019-09-23 14:51:05 +0800

    操作记录

--- serve/routes/logs.js
+++ serve/routes/logs.js
@@ -1,11 +1,52 @@
 var express = require('express');
 var router = express.Router();
-const BasicConfig = require('../models/BasicConfig.js');
+const Logs = require('../models/Logs.js');
 const tool = require('../utils/tool.js');
 /*
 * 查询操作记录*/
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+router.post('/list', function(req, res, next) {
+    let pageNum = parseInt(req.body.pageNum);
+    let pageSize = parseInt(req.body.pageSize);
+    let pageCount = 0;
+
+    Logs.count({
+        status:1
+    },(err,count)=>{
+        if(err){
+            res.json({
+                status:0,
+                message:err,
+                result:{}
+            })
+        }
+        pageCount = count;
+    });
+
+    let log = Logs.find({
+        status:1
+    },{
+        _id:0,
+        __v:0
+    });
+    log.sort({createTime:-1});
+    log.skip((pageNum-1)*pageSize);
+    log.limit(pageSize);
+
+    log.exec((err,logs)=>{
+        if(err){
+            res.json({
+                status:0,
+                message:err,
+                result:{}
+            })
+        }
+        res.json({
+            status:1,
+            message:'查询成功',
+            pageCount:pageCount,
+            result:logs,
+        })
+    });
 });
 
 module.exports = router;
